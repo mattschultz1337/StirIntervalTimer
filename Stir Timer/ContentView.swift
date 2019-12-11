1eaf0084c20b6904f6644611e60692604db58e6e
Matthew Schultz <mschult7@bingha>
2019-12-11 13:27:40 -0500

    set up of project, starting to get timer to work

--- Stir Timer/ContentView.swift
+++ Stir Timer/ContentView.swift
@@ -9,9 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    
+    @State var curr: Date = Date()
+    var referenceDate: Date = Date(timeIntervalSinceNow: 100)
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+            self.curr = Date()
+        }
     }
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(timerLogic(from: referenceDate, to: curr)).font(.largeTitle)
+                Button(action: {
+                    
+                    let _ = self.timer
+                }) {
+                    Text("START").font(.subheadline)
+                }
+            }
+        }.navigationBarTitle("Stir Timer")
+        
+    }
+}
+
+func timerLogic(from date: Date, to curr: Date) -> String{
+    
+    let calendar = Calendar(identifier: .gregorian)
+    let components = calendar
+        .dateComponents([.hour, .minute, .second]
+            ,from: curr,
+             to: date)
+    return String(format: "%02dh:%02d:%02ds",
+    components.hour ?? 00,
+    components.minute ?? 00,
+    components.second ?? 00)
 }
 
 struct ContentView_Previews: PreviewProvider {
