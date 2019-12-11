fb83b4a7ece48775e8af7bb1184606c5674fe24a
Matthew Schultz <mschult7@bingha>
2019-12-11 12:44:55 -0500

    Initial Commit

---
+++ Stir Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stir Timer
+//
+//  Created by Mobile Apps on 12/11/19.
+//  Copyright © 2019 Mobile Apps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
